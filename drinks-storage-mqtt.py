cc657580acca1c7e3da3c47a32a744dc273ee877
and… <jevestops@gmail.com>
2017-07-27 23:08:31 +0200

    Implement core functionality

--- drinks-storage-mqtt.py
+++ drinks-storage-mqtt.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3
-from flask import Flask
 import paho.mqtt.client as mqtt
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "hello"
+tara = 9024900
 
 
-def connection(client, userdata, flags, result):
-    print("hallo"+str(result))
+def on_connect(client, userdata, flags, result):
+    client.subscribe("drinks_storage/scale_measurements/#")
+
+
+def on_message(client, userdata, message):
+    raw_value = int(message.payload)
+    output_value = round((raw_value - tara) / -263600)
+    topic = remove_prefix("drinks_storage/scale_measurements/", message.topic)
+    client.publish("drinks_storage/crate_counts/" + topic, str(output_value))
 
 client = mqtt.Client()
-client.on_connect = connection
-client.connect("192.168.3.140")
+client.on_connect = on_connect
+client.on_message = on_message
 
+client.connect("192.168.3.140")
 client.loop_forever()
 
+
+def remove_prefix(prefix, text):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    else:
+        return text
