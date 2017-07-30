5af7febdb65c49f126111f7c4365c92792f81216
and… <jevestops@gmail.com>
2017-07-30 21:08:18 +0200

    Use config file

--- drinks-storage-mqtt.py
+++ drinks-storage-mqtt.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 import paho.mqtt.client as mqtt
+import yaml
 
-tara = 9024900
+config = yaml.load(open("config.yaml", "r"))
+
+def remove_prefix(prefix, text):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    else:
+        return text
 
 
 def on_connect(client, userdata, flags, result):
@@ -9,21 +16,20 @@ def on_connect(client, userdata, flags, result):
 
 
 def on_message(client, userdata, message):
-    raw_value = int(message.payload)
-    output_value = round((raw_value - tara) / -263600)
-    topic = remove_prefix("drinks_storage/scale_measurements/", message.topic)
-    client.publish("drinks_storage/crate_counts/" + topic, str(output_value))
+    try:
+        topic = remove_prefix("drinks_storage/scale_measurements/", message.topic)
+        tare = config[topic]["tare"]
+        crate_weight = config[topic]["crate_weight"]
+        raw_value = int(message.payload)
+        output_value = round((raw_value - tare) / crate_weight)
+        client.publish("drinks_storage/crate_counts/" + topic, str(output_value))
+    except KeyError as e:
+        print("unknown scale " + str(e))
+    
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("192.168.3.140")
+client.connect("192.168.3.189")
 client.loop_forever()
-
-
-def remove_prefix(prefix, text):
-    if text.startswith(prefix):
-        return text[len(prefix):]
-    else:
-        return text
