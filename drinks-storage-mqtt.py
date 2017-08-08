29077793d7cd86b0dfa9f8a6ed4a07b53394c68d
Felix Möller <git@felix-moeller.net>
2017-08-09 01:28:28 +0200

    Fix topic names

--- drinks-storage-mqtt.py
+++ drinks-storage-mqtt.py
@@ -4,9 +4,12 @@ import yaml
 
 config = yaml.load(open("config.yaml", "r"))
 
+MQTT_TOPIC_RAW = "sensors/cellar/drinks_scale_measurements_raw"
+MQTT_TOPIC_CRATES = "sensors/cellar/drinks_crate_counts"
+
 
 def on_connect(client, userdata, flags, result):
-    client.subscribe("drinks_storage/scale_measurements")
+    client.subscribe(MQTT_TOPIC_RAW)
 
 
 def on_message(client, userdata, message):
@@ -14,11 +17,15 @@ def on_message(client, userdata, message):
         msg_content = yaml.load(message.payload)
 
         scale_config = config["scales"][msg_content["esp_id"]]
-        output_value = round(
-            (msg_content["scale_value"] - scale_config["tare"]) / scale_config["crate_weight"])
+        output_value = round((msg_content["scale_value"]
+                             - scale_config["tare"])
+                             / scale_config["crate_weight"])
 
-        client.publish("drinks_storage/crate_counts/" +
-                       scale_config["scale_name"], str(output_value))
+        client.subscribe(MQTT_TOPIC_RAW)
+        # TODO Spoof ID for each scale to be able to publish to a single topic?
+        client.publish("{}/{}".format(MQTT_TOPIC_CRATES,
+                                      scale_config["scale_name"]),
+                       str(output_value))
     except KeyError as e:
         print("unknown scale " + str(e))
 
