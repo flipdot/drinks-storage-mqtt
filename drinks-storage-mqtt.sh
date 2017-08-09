36a8a1d2c039179a70760b45b1694ad5a44dfbef
Felix Möller <git@felix-moeller.net>
2017-08-09 03:22:36 +0200

    Fix shell wrapper

--- drinks-storage-mqtt.sh
+++ drinks-storage-mqtt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Execute $PY_SCRIPT if not already running
 PY_SCRIPT='drinks-storage-mqtt.py'
-pgrep -a python | grep "$PY_SCRIPT" || exec "$(dirname $0)/$PY_SCRIPT"
+pgrep -a python | grep "$PY_SCRIPT" || (cd "$(dirname $0)" && exec "./$PY_SCRIPT")
