48d7101f75a44acd40a1c16b940fe32978ceed7d
and… <jevestops@gmail.com>
2017-08-13 18:26:05 +0200

    Add measurement range checking

--- drinks-storage-mqtt.py
+++ drinks-storage-mqtt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import paho.mqtt.client as mqtt
 import json
+
+import paho.mqtt.client as mqtt
 import yaml
 
 config = yaml.load(open("config.yaml", "r"))
@@ -19,19 +20,29 @@ def on_message(client, userdata, message):
         msg_content = yaml.load(message.payload)
 
         scale_config = config["scales"][msg_content["esp_id"]]
-        output_value = round((msg_content["scale_value"]
-                             - scale_config["tare"])
-                             / scale_config["crate_weight"])
-        output_json = {
-            "esp_id": str(msg_content["esp_id"]),
-            "crates": str(output_value),
-        }
-        client.publish(MQTT_TOPIC_CRATES, json.dumps(output_json))
+        scale_value_kg = (msg_content["scale_value"] - scale_config["tare"]) \
+                         / scale_config["kilogram"]
+        crates_float = scale_value_kg / scale_config["crate_weight"]
+        crates_int = round(crates_float)
+        diff_kg = abs(crates_float - crates_int) * scale_config["crate_weight"]
+        if diff_kg > scale_config["tolerance"]:
+            error_json = {
+                "origin": "drinks-storage-mqtt",
+                "message": "{}: Measurement not in range, difference = {} kg"
+                           .format(scale_config["scale_name"], diff_kg)
+            }
+            client.publish(MQTT_TOPIC_ERRORS, json.dumps(error_json))
+        else:
+            output_json = {
+                "esp_id": str(msg_content["esp_id"]),
+                "crates": str(crates_int),
+            }
+            client.publish(MQTT_TOPIC_CRATES, json.dumps(output_json))
 
-    except KeyError as e:
+    except KeyError as key:
         error_json = {
             "origin": "drinks-storage-mqtt",
-            "message": "unknown scale " + str(e),
+            "message": "unknown scale " + str(key),
         }
         client.publish(MQTT_TOPIC_ERRORS, json.dumps(error_json))
 
