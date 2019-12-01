d1b11cf677de554a280111cc638434238825f127
Philipp Mandler <info@philipp-mandler.com>
2019-12-02 00:44:42 +0100

    main: format using black

--- drinks_storage/drinks_storage_mqtt.py
+++ drinks_storage/drinks_storage_mqtt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
 import paho.mqtt.client as mqtt
@@ -26,13 +27,10 @@ def send_mqtt(topic, message):
 
 
 ERROR_MESSAGES = {
-    ERROR_OUT_OF_RANGE:
-    lambda config, res:
-    "{}: Measurement not in range, difference = {:.2} crates".format(
-        config.scale_name, res.diff),
-    ERROR_COUNT_NEGATIVE:
-    lambda config, res:
-    f"{config.scale_name}: Crate count negative: {res['crate_count_float']}",
+    ERROR_OUT_OF_RANGE: lambda config, res: "{}: Measurement not in range, difference = {:.2} crates".format(
+        config.scale_name, res.diff
+    ),
+    ERROR_COUNT_NEGATIVE: lambda config, res: f"{config.scale_name}: Crate count negative: {res['crate_count_float']}",
 }
 
 
@@ -52,32 +50,41 @@ def on_message(client, userdata, message):
         send_mqtt(MQTT_TOPIC_ERRORS, json.dumps(error_json))
         return
 
-    (error_code, result) = handle_scale_value(scale_config, scale_value)
+    (error_code, result) = handle_scale_value(
+        scale_config, scale_value, auto_tare=config.config.auto_tare != None
+    )
 
     if error_code < 0:
         send_mqtt(
             MQTT_TOPIC_ERRORS,
-            json.dumps({
-                "origin": "drinks-storage-mqtt",
-                "message": ERROR_MESSAGES[error_code](config, result),
-                "crate_count_float": result["crate_count_float"],
-                "accuracy": result["accuracy"],
-                "diff": result["diff"],
-            }))
+            json.dumps(
+                {
+                    "origin": "drinks-storage-mqtt",
+                    "message": ERROR_MESSAGES[error_code](config, result),
+                    "crate_count_float": result["crate_count_float"],
+                    "accuracy": result["accuracy"],
+                    "diff": result["diff"],
+                }
+            ),
+        )
     else:
         send_mqtt(
             MQTT_TOPIC_CRATES,
-            json.dumps({
-                "scale_name": scale_config.scale_name,
-                "crate_count": result["crate_count"],
-                "crate_count_float": result["crate_count_float"],
-                "accuracy": result["accuracy"],
-                "diff": result["diff"],
-            }))
+            json.dumps(
+                {
+                    "scale_name": scale_config.scale_name,
+                    "crate_count": result["crate_count"],
+                    "crate_count_float": result["crate_count_float"],
+                    "accuracy": result["accuracy"],
+                    "diff": result["diff"],
+                }
+            ),
+        )
 
-        # Rewrite config on auto tare
-        if config.config.auto_tare.rewrite_cfg and result["auto_tared"]:
-            save_config()
+    # Rewrite config on auto tare
+    if config.config.auto_tare.rewrite_cfg and result["auto_tared"]:
+        logging.debug("Autotare changed config, saving...")
+        save_config()
 
 
 def main():
